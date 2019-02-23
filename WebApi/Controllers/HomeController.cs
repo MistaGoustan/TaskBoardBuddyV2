f7032178f250906ae9eeb63c5118c3454b692857
MistaGoustan <costanzawilliam3@gmail.com>
2019-02-23 14:58:10 -0500

    removed privacy policy page configured _Layout as well as made all loading of scripts not based on environment. because its self hosed it doesnt matter if i goes right to the "local" path

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -6,33 +6,7 @@ namespace TaskBoardBuddy.API.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ITaskRepository _taskRepository;
-
-        //public HomeController(ITaskRepository taskRepository)
-        //    => _taskRepository = taskRepository ?? throw new ArgumentNullException(nameof(taskRepository));
-
-        //[HttpGet("{filterState?}", Name = "default")]
-        //public IActionResult Index(string filterState)
-        //{
-        //    if (string.IsNullOrEmpty(filterState) || filterState == "ALL")
-        //        filterState = string.Empty;
-
-        //    var viewModel = new TaskBoardViewModel()
-        //    {
-        //        FilterState = filterState,
-        //        Tasks = _taskRepository.GetTasksByState(filterState)
-        //    };
-
-        //    return View(viewModel);
-        //}
-
-        // DEFAULT CODE
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
